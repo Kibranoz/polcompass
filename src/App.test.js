6f35b53ae330b3a2969be47ab6835fe06f8a4971
Kibranoz <35783229+Kibranoz@users.noreply.github.com>
2025-03-30 15:01:29 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+
+ 
+
+test('renders learn react link', () => {
+  render(<App />);
+  expect(true).toBe(true);
+});
