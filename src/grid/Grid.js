c5f73c011041f62990e0c2fd8ad3593f2a1f1116
Louis Couture <louisr.couture@gmail.com>
2025-03-31 11:46:19 -0400

    Update Grid.js

--- src/grid/Grid.js
+++ src/grid/Grid.js
@@ -1,15 +1,12 @@
 import { render } from "@testing-library/react";
-import { Component, Fragment } from "react";
-import compass from "./compass4.jpg";
+import { Component } from "react";
 import "./Grid.css";
 
 class Grid extends Component {
   constructor(props) {
     super(props);
-    this.state = { ballX: 10, ballY: 20 };
+    this.state = { ballX: 0, ballY: 0 };
   }
-
-  //TODO; réduire limage
 
   componentDidUpdate() {
     const canvas = document.getElementById("myCanvas");
@@ -17,11 +14,7 @@ class Grid extends Component {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.beginPath();
     this.drawPolCompass();
-
-    ctx.fillStyle = "white";
-    ctx.arc(this.state.ballX, this.state.ballY, 10, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
+    this.drawBallAtPosition();
   }
 
   drawPolCompass() {
@@ -30,47 +23,52 @@ class Grid extends Component {
 
     // Define the colors for the four squares
     const colors = [
-      "magenta", // Top-left
-      "blue", // Top-right
+      "#f72525", // Top-left
+      "#2584f7", // Top-right
       "lightgreen", // Bottom-left
       "yellow", // Bottom-right
     ];
 
-    // Calculate the dimensions of each square
     const squareWidth = canvas.width / 2;
     const squareHeight = canvas.height / 2;
 
-    // Function to draw a single square
     function drawSquare(x, y, color) {
       ctx.fillStyle = color;
       ctx.fillRect(x, y, squareWidth, squareHeight);
-      ctx.strokeStyle = "black"; // Add black border
-      ctx.lineWidth = 2; // Set border width
-      ctx.strokeRect(x, y, squareWidth, squareHeight); // Draw the border
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
+      ctx.strokeRect(x, y, squareWidth, squareHeight);
     }
-
-    // Draw the four squares
     drawSquare(0, 0, colors[0]);
     drawSquare(squareWidth, 0, colors[1]);
     drawSquare(0, squareHeight, colors[2]);
     drawSquare(squareWidth, squareHeight, colors[3]);
   }
+
+  drawBallAtPosition() {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.arc(this.state.ballX, this.state.ballY, 5, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+  }
   componentDidMount() {
     this.drawPolCompass();
+    this.drawBallAtPosition();
   }
   render() {
     this.state = {
-      ballX: this.props.economic * (200 / 11) + 200,
-      ballY: -1 * this.props.social * (200 / 13) + 200,
+      ballX: this.props.economic * (200 / 24) + 200,
+      ballY: -1 * this.props.social * (200 / 26) + 200,
     };
-    console.log("state", this.state);
     return (
       <div className="grid-section">
         <div className="grid-area">
           <canvas id="myCanvas" width="400" height="400"></canvas>
         </div>
         <div>
-          {this.props.economic} , {this.props.social}{" "}
+          {this.props.economic} , {this.props.social}
         </div>
       </div>
     );
