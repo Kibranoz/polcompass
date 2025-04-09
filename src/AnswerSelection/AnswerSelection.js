ffb64c378e29f1ea5497f59cf9ddab2b73459329
louiscouture <louisr.couture@gmail.com>
2025-04-09 19:05:03 -0400

    remove comments

--- src/AnswerSelection/AnswerSelection.js
+++ src/AnswerSelection/AnswerSelection.js
@@ -58,26 +58,3 @@ export default function AnswerSelection({onAnswerSelected}) {
 
 
 }
-/**class AnswerSelection extends Component{
-    constructor(props) {
-        super(props);
-      }
-      chooseAppropriateClickHandler = (answer, intensity) => {
-        return this.props.clickHandlerFunctions[this.props.question["affects"]].bind(this,answer*intensity*Number(this.props.question["direction"]))
-      }
-      render(){
-        return(
-          <div className = "answerArea">
-            <Fragment>
-                <AnswerButton answer="Totally agree" questionHandler = {this.nextQuestion} color = "seagreen" clickHandler={this.chooseAppropriateClickHandler(1,2)}></AnswerButton>
-                <AnswerButton answer="Agree" questionHandler = {this.nextQuestion} clickHandler={this.chooseAppropriateClickHandler(1,1)} color="darkseagreen"></AnswerButton>
-                <AnswerButton answer="Unsure/Neutral" questionHandler = {this.nextQuestion} color = "sandybrown"></AnswerButton>
-                <AnswerButton answer="Disagree" questionHandler = {this.nextQuestion} color = "lightcoral" clickHandler={this.chooseAppropriateClickHandler(-1,1)}></AnswerButton>
-                <AnswerButton answer="Totally Disagree" questionHandler = {this.nextQuestion} color = "indianred" clickHandler={this.chooseAppropriateClickHandler(-1,2)}></AnswerButton>
-            </Fragment>
-            </div>
-        )
-      }
-    }
-      **/
-
